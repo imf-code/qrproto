8a0af4438294a3b7a8a74b2a717c0618cb108a06
imf-code <55207685+imf-code@users.noreply.github.com>
2022-01-24 19:09:21 +0200

    cleanup + added readme

--- standalone/kamera.js
+++ standalone/kamera.js
@@ -1,16 +1,16 @@
 // Grab HTML elements
 const canvas = document.querySelector('canvas');
-const video = document.querySelector('video');
 const button = document.querySelector('button');
+const video = document.querySelector('video');
 
-// Init
-button.onclick = readqr;
+// Bind controls
+button.onclick = readQR;
 
 // Global variables
 const ctx = canvas.getContext('2d');
 let stream;
 
-// Setup preview
+// Setup <video> preview of camera feed (not actually necessary)
 const constraints = window.constraints = {
     audio: false,
     video: true
@@ -23,65 +23,45 @@ navigator.mediaDevices.getUserMedia(constraints)
     })
     .catch(err => console.log(err));
 
-/** Start reading QR */
-function readqr() {
-    const vTrack = stream.getVideoTracks()[0];
-    const frame = new ImageCapture(vTrack);
+/** Start looking for a QR code */
+function readQR() {
+    const videoTrack = stream.getVideoTracks()[0]; // Assumes track 0 is the right camera
+    const trackCapture = new ImageCapture(videoTrack);
 
-    frame.grabFrame()
-        .then(function (bitmap) {
-
-            canvas.width = bitmap.width;
-            canvas.height = bitmap.height;
-
-            // Draw frame on canvas + aiming rect 
-            canvas.getContext('2d').drawImage(bitmap, 0, 0);
-            const imgData = ctx.getImageData(120, 40, 400, 400);
-            ctx.beginPath();
-            ctx.rect(120, 40, 400, 400);
-            ctx.stroke();
-
-            const result = jsQR(imgData.data, 400, 400);
-
-            // Loop until qr is read
-            if (result) {
-                console.log(result.data);
-                return;
-            }
-            else {
-                setTimeout(readqr, 1000 / 30);
-            }
-        })
-        .catch(err => console.log(err));
+    readLoop(trackCapture);
 }
 
-/**  */
-function readloop() {
-    const vTrack = stream.getVideoTracks()[0];
-    const frame = new ImageCapture(vTrack);
+/** Update canvas preview and attempt to read QR until one is found 
+ * @param capturedTrack An ImageCapture of a MediaStreamTrack
+*/
+function readLoop(capturedTrack) {
 
-    frame.grabFrame()
+    capturedTrack.grabFrame()
         .then(function (bitmap) {
 
+            // Draw a frame on canvas + aiming rectangle
             canvas.width = bitmap.width;
             canvas.height = bitmap.height;
+            
+            const rectX = (bitmap.width - 400) / 2;
+            const rectY = (bitmap.height - 400) / 2;
 
-            // Draw frame on canvas + aiming rect 
             canvas.getContext('2d').drawImage(bitmap, 0, 0);
-            const imgData = ctx.getImageData(120, 40, 400, 400);
             ctx.beginPath();
-            ctx.rect(120, 40, 400, 400);
+            ctx.rect(rectX, rectY, 400, 400);
             ctx.stroke();
 
+            // Attempt to find a QR code inside aiming rect
+            const imgData = ctx.getImageData(rectX, rectY, 400, 400);
             const result = jsQR(imgData.data, 400, 400);
 
-            // Loop until qr is read
+            // Loop until QR is found
             if (result) {
                 console.log(result.data);
                 return;
             }
             else {
-                setTimeout(readqr, 1000 / 24);
+                setTimeout(() => readQR(capturedTrack), 1000 / 30); // 30fps
             }
         })
         .catch(err => console.log(err));
