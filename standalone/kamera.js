fdcfc1bc506c004c4a55157c5ad1276302367857
imf-code <55207685+imf-code@users.noreply.github.com>
2022-01-24 18:08:01 +0200

    working now

--- standalone/kamera.js
+++ standalone/kamera.js
@@ -4,7 +4,7 @@ const video = document.querySelector('video');
 const button = document.querySelector('button');
 
 // Init
-button.onclick = snapshot;
+button.onclick = readqr;
 
 // Global variables
 const ctx = canvas.getContext('2d');
@@ -23,8 +23,8 @@ navigator.mediaDevices.getUserMedia(constraints)
     })
     .catch(err => console.log(err));
 
-/** Take a snapshot */
-function snapshot() {
+/** Start reading QR */
+function readqr() {
     const vTrack = stream.getVideoTracks()[0];
     const frame = new ImageCapture(vTrack);
 
@@ -34,16 +34,54 @@ function snapshot() {
             canvas.width = bitmap.width;
             canvas.height = bitmap.height;
 
+            // Draw frame on canvas + aiming rect 
             canvas.getContext('2d').drawImage(bitmap, 0, 0);
-            const imgData = ctx.getImageData(220, 140, 200, 200);
-            const result = jsQr(imgData, 200, 200);
+            const imgData = ctx.getImageData(120, 40, 400, 400);
+            ctx.beginPath();
+            ctx.rect(120, 40, 400, 400);
+            ctx.stroke();
 
+            const result = jsQR(imgData.data, 400, 400);
+
+            // Loop until qr is read
             if (result) {
                 console.log(result.data);
                 return;
             }
             else {
-                setTimeout(snapshot, 1000 / 10);
+                setTimeout(readqr, 1000 / 30);
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+/**  */
+function readloop() {
+    const vTrack = stream.getVideoTracks()[0];
+    const frame = new ImageCapture(vTrack);
+
+    frame.grabFrame()
+        .then(function (bitmap) {
+
+            canvas.width = bitmap.width;
+            canvas.height = bitmap.height;
+
+            // Draw frame on canvas + aiming rect 
+            canvas.getContext('2d').drawImage(bitmap, 0, 0);
+            const imgData = ctx.getImageData(120, 40, 400, 400);
+            ctx.beginPath();
+            ctx.rect(120, 40, 400, 400);
+            ctx.stroke();
+
+            const result = jsQR(imgData.data, 400, 400);
+
+            // Loop until qr is read
+            if (result) {
+                console.log(result.data);
+                return;
+            }
+            else {
+                setTimeout(readqr, 1000 / 24);
             }
         })
         .catch(err => console.log(err));
